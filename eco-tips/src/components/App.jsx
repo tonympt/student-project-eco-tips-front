67ed9705f6825295b1ab18d45e749e17ed30536e
tonympt <tony.memponteil@gmail.com>
2023-04-12 16:23:09 +0200

    simple project structure

---
+++ eco-tips/src/components/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <div>
+      <h1 class="text-3xl underline font-bold">Vite + React</h1>
+    </div>
+  )
+}
+
+export default App
--- eco-tips/src/main.jsx
+++ eco-tips/src/main.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import {BrowserRouter} from 'react-router-dom'
+
+
+import App from '../src/components/App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
