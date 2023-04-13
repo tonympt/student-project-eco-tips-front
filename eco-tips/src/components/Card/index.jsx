915b80ceb90bd92eff2d9262391b1f45c3d8b728
tonympt <tony.memponteil@gmail.com>
2023-04-13 20:33:36 +0200

    feature zoom on card component

--- eco-tips/src/components/Card/index.jsx
+++ eco-tips/src/components/Card/index.jsx
@@ -1,6 +1,8 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
+
 // react and redux hooks
+import { useState, useEffect, useRef } from 'react';
 
 import PropTypes from 'prop-types';
 // components
@@ -13,22 +15,64 @@ import CardImg from './CardImg';
 import CheckIcon from './CheckIcon';
 
 function Card({ imageSrc, title, tags, description, author, ratings, validated }) {
+  const [isExpanded, setIsExpanded] = useState(false);
+  const [showBackground, setShowBackground] = useState(false);
+  const cardRef = useRef();
+
+  const styleCardExpanded = isExpanded
+    ? 'z-40 fixed top-1/2 left-1/2 animate-expand cursor-auto w-full md:w-1/4'
+    : 'md:w-48 cursor-pointer';
+  const styleValidated = validated && 'border-4 border-green-600';
+
+  const handleClick = () => {
+    setIsExpanded(true);
+    setShowBackground(true);
+  };
+
+  const handleOutsideClick = (event) => {
+    // Check if the clicked element is not a descendant of cardRef and isExpanded=true
+    if (!cardRef.current.contains(event.target) && isExpanded) {
+      setIsExpanded(false);
+      setShowBackground(false);
+    }
+  };
+
+  useEffect(() => {
+    // listen click on window when isExpanded=true
+    window.addEventListener('click', handleOutsideClick);
+    return () => {
+      window.removeEventListener('click', handleOutsideClick);
+    };
+  }, [isExpanded]);
+
   return (
-    <div className="relative">
-      <div
-        className="bg-white rounded shadow-md hover:shadow-lg sm:w-full "
-      >
-        <CardImg imageSrc={imageSrc} title={title} />
-        <div className="p-4">
-          <CardTitle title={title} />
-          <CardTags tags={tags} />
-          <CardRating ratings={ratings} />
-          <CardDescription description={description} />
-          <CardAuthor author={author} />
-          {validated && <CheckIcon />}
+    <>
+      {showBackground && (
+        <button
+          className="z-10 fixed inset-0 bg-black bg-opacity-20 backdrop-blur-md"
+          type="button"
+          aria-label="Outside click handler"
+          onClick={handleOutsideClick}
+        />
+      )}
+      <div className="relative" aria-label="Card container">
+        <div
+          ref={cardRef}
+          className={`bg-white rounded shadow-md hover:shadow-lg sm:w-full ${styleCardExpanded} ${styleValidated}`}
+          onClick={handleClick}
+        >
+          <CardImg imageSrc={imageSrc} title={title} />
+          <div className="p-4">
+            <CardTitle title={title} isExpanded={isExpanded} />
+            <CardTags tags={tags} />
+            <CardRating ratings={ratings} />
+            <CardDescription description={description} isExpanded={isExpanded} />
+            <CardAuthor author={author} />
+            {validated && <CheckIcon />}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
