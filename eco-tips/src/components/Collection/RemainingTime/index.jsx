8d29bcee1f1f3c723d358608a5882a6551b633e4
SebastienDurand1 <sebastien_durand@outlook.fr>
2023-04-18 16:54:59 +0200

    icon added

--- eco-tips/src/components/Collection/RemainingTime/index.jsx
+++ eco-tips/src/components/Collection/RemainingTime/index.jsx
@@ -2,7 +2,6 @@
 
 import dayjs from 'dayjs';
 
-
 function DisplayRemainingTime() {
   // récupèrer la targetDate de la carte
   const targetDate = '12'; // nombre factice
@@ -19,8 +18,12 @@ function DisplayRemainingTime() {
   const remainingDays = timeRemaining();
   return (
 
-    <div className="flex flex-row items-center ">
-      <p className="text-sm font-semibold">
+    <div className="flex flex-row text-center w-full mx-auto bg-white p-4 rounded-md shadow-md relative mt-3 ">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+      </svg>
+
+      <p className="text-sm font-semibold text-center ml-2 ">
         {' '}
         {remainingDays}
         {' '}
